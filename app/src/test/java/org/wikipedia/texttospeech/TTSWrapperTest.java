fe455a4efbf6e1de60d21fa953852216ce234664
Frederic Daigle <Freddydaigle@gmail.com>
2018-02-05 21:50:41 -0500

    Implemented first TTSWrapper test

---
+++ app/src/test/java/org/wikipedia/texttospeech/TTSWrapperTest.java
@@ -0,0 +1,48 @@
+package org.wikipedia.texttospeech;
+
+/**
+ * Created by Fred on 2018-02-05.
+ */
+import android.content.Context;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import org.mockito.runners.MockitoJUnitRunner;
+import org.wikipedia.test.TestRunner;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class TTSWrapperTest {
+
+    @Mock
+    TextToSpeech tts = mock(TextToSpeech.class);
+
+    @Mock
+    Context context = mock(Context.class);
+
+    @Mock
+    TextToSpeech.OnInitListener listener = mock(TextToSpeech.OnInitListener.class);
+
+    @InjectMocks
+    TTSWrapper ttsWrapper = TTSWrapper.getInstance(context, listener);;
+
+    @Before
+    public void setUp() throws Throwable {
+        ttsWrapper.setTTS(tts);
+        when(context.toString()).thenReturn("context");
+    }
+
+    @Test
+    public void testSpeak() {
+        ttsWrapper.speak("Test");
+        verify(tts).speak("Test", TextToSpeech.QUEUE_ADD, null,  "0");
+    }
+}
