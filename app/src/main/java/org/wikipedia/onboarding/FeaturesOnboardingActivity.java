03a54ef1ee57d58800b225c658b102f545c4b464
xTEddie <edwardtranv1@gmail.com>
2018-02-09 19:05:15 -0500

    Add basic FeaturesOnBoarding component

---
+++ app/src/main/java/org/wikipedia/onboarding/FeaturesOnboardingActivity.java
@@ -0,0 +1,35 @@
+package org.wikipedia.onboarding;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import org.wikipedia.activity.SingleFragmentActivity;
+import org.wikipedia.settings.Prefs;
+
+public class FeaturesOnboardingActivity
+        extends SingleFragmentActivity<FeaturesOnboardingFragment>
+        implements FeaturesOnboardingFragment.Callback {
+
+    @NonNull public static Intent newIntent(@NonNull Context context) {
+        return new Intent(context, FeaturesOnboardingActivity.class);
+    }
+
+    @Override public void onComplete() {
+        setResult(RESULT_OK);
+        Prefs.setInitialOnboardingEnabled(false);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragment().onBackPressed()) {
+            return;
+        }
+        finish();
+    }
+
+    @Override protected FeaturesOnboardingFragment createFragment() {
+        return FeaturesOnboardingFragment.newInstance();
+    }
+}
