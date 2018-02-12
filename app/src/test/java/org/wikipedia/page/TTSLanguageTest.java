106767371959383582665562d9a7ebf059962374
kammyWong <kammy609@msn.com>
2018-02-12 03:06:35 -0500

    Add tests for connect UI with language detection #20

--- app/src/test/java/org/wikipedia/page/TTSLanguageTest.java
+++ app/src/test/java/org/wikipedia/page/TTSLanguageTest.java
@@ -1,8 +1,14 @@
 package org.wikipedia.page;
+import android.speech.tts.TextToSpeech;
+
 import org.junit.Test;
+import org.wikipedia.language.AppLanguageLookUpTable;
+
 import java.util.Locale;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 /** Unit tests for TTS Language. */
 public class TTSLanguageTest {
@@ -18,5 +24,16 @@ public class TTSLanguageTest {
         Locale loc2 = page.getLocaleForTTS("Spanish");
         assertEquals(loc2.getCountry(),"PA");
     }
+    @Test
+    public void testSetLanguageName(){
+        PageActivity page = new PageActivity();
 
+        //Different types of written language Chinese are set to be Chinese
+        String language = page.setLanguageName("Traditional Chinese");
+        assertEquals(language,"Chinese");
+
+        //Wiki languages do not contain different type of French, so it still returns French
+        language = page.setLanguageName("French");
+        assertEquals(language,"French");
+    }
 }
