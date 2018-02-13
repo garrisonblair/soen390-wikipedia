6658efb2813eee8b1e06bbcdfbf77b3196d6ef1a
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-02-13 14:27:02 -0500

    created HideStopButtonOnDoneListener class #25

---
+++ app/src/main/java/org/wikipedia/page/listeners/HideStopButtonOnDoneListener.java
@@ -0,0 +1,32 @@
+package org.wikipedia.page.listeners;
+
+import android.speech.tts.UtteranceProgressListener;
+import android.view.View;
+
+import org.wikipedia.page.PageActivity;
+
+public class HideStopButtonOnDoneListener extends UtteranceProgressListener {
+    private PageActivity pageActivity;
+    public HideStopButtonOnDoneListener(PageActivity pageActivity){
+        this.pageActivity = pageActivity;
+    }
+    @Override
+    public void onStart(String s) {
+
+    }
+
+    @Override
+    public void onDone(String s) {
+        pageActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                pageActivity.getStopButton().setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public void onError(String s) {
+
+    }
+}
