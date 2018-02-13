eff3a892d6bf0a13186685cd76f6eb2f51afd9b3
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-02-13 17:00:25 -0500

    clean files #25

--- app/src/androidTest/java/org/wikipedia/menu/SelectedTextTest.java
+++ app/src/androidTest/java/org/wikipedia/menu/SelectedTextTest.java
@@ -1,32 +1,20 @@
 package org.wikipedia.menu;
 
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
-import android.view.MenuItem;
-import android.view.View;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.wikipedia.R;
-import org.wikipedia.main.MainActivity;
 import org.wikipedia.page.PageActivity;
 
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.Espresso.openContextualActionModeOverflowMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.longClick;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.v4.view.MenuCompat.setShowAsAction;
 
 /**
  * Created by Andres on 2018-02-13.
