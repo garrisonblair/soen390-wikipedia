4fef87754e190fa275325ff373a110d244be4a41
Frederic Daigle <Freddydaigle@gmail.com>
2018-02-12 20:02:03 -0500

    Started adding the TTS preview.  Set up the button with a test string to be spoken #31

---
+++ app/src/main/java/org/wikipedia/texttospeech/TTSPreviewPreference.java
@@ -0,0 +1,44 @@
+package org.wikipedia.texttospeech;
+
+import android.content.Context;
+import android.support.v7.preference.PreferenceViewHolder;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.wikipedia.R;
+
+/**
+ * Created by Fred on 2018-02-12.
+ */
+
+public class TTSPreviewPreference extends android.support.v7.preference.Preference {
+
+    private TTSWrapper tts;
+
+    public TTSPreviewPreference(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+        setWidgetLayoutResource(R.layout.tts_preview_layout);
+        setLayoutResource(R.layout.tts_preview_layout);
+
+        tts = TTSWrapper.getInstance(context, null);
+    }
+
+    public void onClick() {
+
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        Button ttsPreviewBtn = (Button) holder.findViewById(R.id.tts_preview_btn);
+
+        ttsPreviewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tts.speak("Test String");
+            }
+        });
+    }
+}
+
