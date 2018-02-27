410d63024cdbc27f9f445d9a0f6d42f3e7117e96
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-02-27 12:45:36 -0500

    test for GalleryPickUtil.java #59

---
+++ app/src/test/java/org/wikipedia/util/GalleryPickUtilTest.java
@@ -0,0 +1,56 @@
+package org.wikipedia.util;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wikipedia.test.TestRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Andres on 2018-02-26.
+ */
+
+@RunWith(TestRunner.class)
+public class GalleryPickUtilTest {
+
+    @Test
+    public void newGalleryPickIntentTest(){
+        Intent intent = GalleryPickUtil.newGalleryPickIntent();
+        assertEquals("image/*",intent.getType());
+    }
+
+    @Test
+    public void getSelectedPictureTest(){
+        Intent intentMock = mock(Intent.class);
+        Uri uri = mock(Uri.class);
+
+        //constant should be -1
+        assertEquals(Activity.RESULT_OK, -1);
+        final int OK = Activity.RESULT_OK;
+
+        //constant should be 0
+        assertEquals(Activity.RESULT_CANCELED, 0);
+        final int CANCEL = Activity.RESULT_CANCELED;
+
+        when(intentMock.getData()).thenReturn(uri);
+        Activity activityMock = mock(Activity.class);
+
+        Bitmap bitmap = GalleryPickUtil.getSelectedPicture(OK, null,activityMock);
+        assertEquals(bitmap,null);
+
+        bitmap = GalleryPickUtil.getSelectedPicture(CANCEL, intentMock,activityMock);
+        assertEquals(bitmap,null);
+
+        bitmap = GalleryPickUtil.getSelectedPicture(OK, intentMock, activityMock);
+        assertTrue(bitmap != null);
+    }
+}
