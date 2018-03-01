a89b05612c06815e2d2295f9e785c3622f76d092
kammyWong <kammy609@msn.com>
2018-02-28 23:16:35 -0500

    Add test to verify behaviours #54

--- app/src/test/java/org/wikipedia/util/CameraUtilTest.java
+++ app/src/test/java/org/wikipedia/util/CameraUtilTest.java
@@ -5,15 +5,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.wikipedia.settings.Prefs;
 import org.wikipedia.settings.PrefsIoUtil;
+import org.wikipedia.texttospeech.TTSWrapper;
 import org.wikipedia.util.CameraUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
@@ -24,6 +31,7 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,6 +43,23 @@ public class CameraUtilTest {
         assertEquals(null, cameraUtil.getPath());
         cameraUtil.setPath("wikipedia");
         assertEquals("wikipedia",cameraUtil.getPath());
+    }
+
+    @Test
+    public void testGetPublicStorageDirectory (){
+        Context mockContext = mock(Context.class);
+        CameraUtil mockCameraUtil = mock(CameraUtil.class);
+        File file = mockCameraUtil.getPublicStorageDirectory(mockContext);
+        verify(mockCameraUtil).getPublicStorageDirectory(mockContext);
+    }
+
+    @Test
+    public void testAddPhoto(){
+        CameraUtil mockCameraUtil = mock(CameraUtil.class);
+        Context mockContext = mock(Context.class);
+        String path = "wikipedia";
+        mockCameraUtil.addPhotoToGallery(mockContext, path);
+        verify(mockCameraUtil).addPhotoToGallery(mockContext, path);
     }
 
 }
