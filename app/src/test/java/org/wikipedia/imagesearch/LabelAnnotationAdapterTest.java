0de993b6ccbaf0829e13dff4d2ac8ae32e74ec04
Steven Locke <tap3stry@gmail.com>
2018-03-01 20:15:20 -0500

    #55 Finished LabelAnnotationAdapterTest

--- app/src/test/java/org/wikipedia/imagesearch/LabelAnnotationAdapterTest.java
+++ app/src/test/java/org/wikipedia/imagesearch/LabelAnnotationAdapterTest.java
@@ -1,36 +1,34 @@
 package org.wikipedia.imagesearch;
 
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 /**
  * Created by steve on 01/03/18.
  */
 public class LabelAnnotationAdapterTest {
     @Test
-    public void getDescription() throws Exception {
+    public void getDescriptionTest() throws Exception {
 
-        /*
-
-        ImageRecognitionService imageRecognitionService = mock(ImageRecognitionService.class);
-        ImageRecognitionService.Callback callback = mock(ImageRecognitionService.Callback.class);
-        imageRecognitionService.setCallback(callback);
-        when(imageRecognitionService.getCallback()).thenReturn(callback);
-        assertEquals(callback, imageRecognitionService.getCallback());
-        ImageRecognitionService imageRecognitionService = new ImageRecognitionService();
-        ImageRecognitionService.Callback callback = mock(ImageRecognitionService.Callback.class);
-        imageRecognitionService.setCallback(callback);
-        assertEquals(callback, imageRecognitionService.getCallback());
-
-        */
+        String testString = "Test String";
+        EntityAnnotation entityAnnotation = new EntityAnnotation();
+        entityAnnotation.setDescription(testString);
+        assertEquals(testString, entityAnnotation.getDescription());
     }
 
     @Test
-    public void getScore() throws Exception {
+    public void getScoreTest() throws Exception {
 
+        Float testScore = (float)9.9;
+        EntityAnnotation entityAnnotation = new EntityAnnotation();
+        entityAnnotation.setScore(testScore);
+        assertEquals(testScore, entityAnnotation.getScore());
 
     }
 
