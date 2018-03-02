481d322c5ced2ee8217b747471ff7222443d766c
Steven Locke <tap3stry@gmail.com>
2018-03-01 19:37:40 -0500

    #55 Adding LabelAnnotationAdapter and LabelAnnotationAdapterTest

--- app/src/main/java/org/wikipedia/imagesearch/LabelAnnotationAdapter.java
+++ app/src/main/java/org/wikipedia/imagesearch/LabelAnnotationAdapter.java
@@ -1,8 +1,30 @@
 package org.wikipedia.imagesearch;
 
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+
 /**
  * Created by steve on 01/03/18.
  */
 
 public class LabelAnnotationAdapter implements ImageRecognitionLabel {
+
+    private EntityAnnotation entityAnnotation;
+
+
+    public LabelAnnotationAdapter(EntityAnnotation entityAnnotation){
+
+        this.entityAnnotation = entityAnnotation;
+    }
+
+    @Override
+    public String getDescription() {
+
+        return entityAnnotation.getDescription();
+    }
+
+    @Override
+    public double getScore() {
+
+        return entityAnnotation.getScore();
+    }
 }
---
+++ app/src/test/java/org/wikipedia/imagesearch/LabelAnnotationAdapterTest.java
@@ -0,0 +1,37 @@
+package org.wikipedia.imagesearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by steve on 01/03/18.
+ */
+public class LabelAnnotationAdapterTest {
+    @Test
+    public void getDescription() throws Exception {
+
+        /*
+
+        ImageRecognitionService imageRecognitionService = mock(ImageRecognitionService.class);
+        ImageRecognitionService.Callback callback = mock(ImageRecognitionService.Callback.class);
+        imageRecognitionService.setCallback(callback);
+        when(imageRecognitionService.getCallback()).thenReturn(callback);
+        assertEquals(callback, imageRecognitionService.getCallback());
+        ImageRecognitionService imageRecognitionService = new ImageRecognitionService();
+        ImageRecognitionService.Callback callback = mock(ImageRecognitionService.Callback.class);
+        imageRecognitionService.setCallback(callback);
+        assertEquals(callback, imageRecognitionService.getCallback());
+
+        */
+    }
+
+    @Test
+    public void getScore() throws Exception {
+
+
+    }
+
+}
