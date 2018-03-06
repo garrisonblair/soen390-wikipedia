e760fd8cbb3720b51310daa7e411a5919b8352eb
Frederic Daigle <Freddydaigle@gmail.com>
2018-03-06 12:22:53 -0500

    #89 Added logic for handling the fact that the anchorNode and focusNode can be on either end of the selection.  If one node is encountered first in the traversal, then the other must be the one to end the selection.

--- app/src/main/assets/getSelectionAndReference.js
+++ app/src/main/assets/getSelectionAndReference.js
@@ -10,28 +10,33 @@ function getNodesInSelection(selection) {
     content = content[0];
     var nodes = [];
 
-    getNodesInSelectionRecursive(content, selection, nodes, 0);
+    getNodesInSelectionRecursive(content, selection, nodes, 0, null);
 
     return [];
 }
 
-function getNodesInSelectionRecursive(node, selection, nodes, phase) {
+function getNodesInSelectionRecursive(node, selection, nodes, phase, endNode) {
     for (var i = 0; i < node.childNodes.length; i++) {
         var childNode = node.childNodes[i];
         if(phase == 0) {
             if(childNode == selection.anchorNode) {
+                endNode = selection.focusNode;
+                nodes.push(childNode);
+                phase = 1;
+            } else if (childNode == selection.focusNode) {
+                endNode = selection.anchorNode;
                 nodes.push(childNode);
                 phase = 1;
             } else {
-                phase = getNodesInSelectionRecursive(childNode, selection, nodes, phase);
+                phase = getNodesInSelectionRecursive(childNode, selection, nodes, phase, endNode);
             }
         } else if (phase == 1) {
             nodes.push(childNode);
-            if (childNode == selection.focusNode) {
+            if (childNode == endNode) {
                 phase = 2;
                 return phase;
             }
-            phase = getNodesInSelectionRecursive(childNode, selection, nodes, phase);
+            phase = getNodesInSelectionRecursive(childNode, selection, nodes, phase, endNode);
 
         } else if (phase == 2) {
             return phase;
