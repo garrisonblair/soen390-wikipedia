828c992e4357e52d66ac3840cbb25c44349352d6
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-03-09 16:49:27 -0500

    #90 DAO for NoteEntity

---
+++ app/src/main/java/org/wikipedia/notebook/database/NoteDao.java
@@ -0,0 +1,29 @@
+package org.wikipedia.notebook.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by Andres on 2018-03-08.
+ */
+@Dao
+public interface NoteDao {
+
+    @Query("SELECT * FROM notes WHERE articleId = :articleId ORDER BY id ASC")
+    public List<NoteEntity> getAllNotesFromArticle(String articleId);
+
+    @Insert
+    public void addNote(NoteEntity note);
+
+    @Query("SELECT ID FROM notes WHERE ID = (SELECT MAX(ID)  FROM notes);")
+    public int getLastInsertedRowId();
+
+    /*@Query("SELECT * FROM notes n , `references` r WHERE n.id = r.noteId")
+    public*/
+
+}
+
