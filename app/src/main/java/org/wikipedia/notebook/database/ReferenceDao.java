7202e5d583f330a858b5a26957ecd17612b03a91
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-03-09 16:50:48 -0500

    #90 DAO for ReferenceEntity

---
+++ app/src/main/java/org/wikipedia/notebook/database/ReferenceDao.java
@@ -0,0 +1,21 @@
+package org.wikipedia.notebook.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by Andres on 2018-03-08.
+ */
+@Dao
+public interface ReferenceDao {
+
+    @Query("SELECT * FROM `references` WHERE articleId = :articleId ORDER BY noteId ASC")
+    public List<ReferenceEntity> getAllArticleReferences(String articleId);
+
+    @Insert
+    public void addReferences(List<ReferenceEntity> references);
+
+}
