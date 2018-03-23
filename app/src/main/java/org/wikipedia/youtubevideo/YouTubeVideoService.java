0926adc708427f1f07f7c265e5cff6fb2187086e
kammyWong <kammy609@msn.com>
2018-03-23 02:50:53 -0400

    #128 Added YouTubeVideoService

---
+++ app/src/main/java/org/wikipedia/youtubevideo/YouTubeVideoService.java
@@ -0,0 +1,90 @@
+package org.wikipedia.youtubevideo;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeRequestInitializer;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by KammyWong on 2018-03-23.
+ */
+
+public class YouTubeVideoService {
+    private YouTube youtube;
+
+    private final String apiKey = "AIzaSyC97eTu0rdwGuoJg0hv1r8No_55iaaeBp4";
+
+    public YouTubeVideoService() {
+        youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest httpRequest) throws IOException {
+            }
+        }).setYouTubeRequestInitializer(new YouTubeRequestInitializer(apiKey)).build();
+    }
+
+    public List<SearchResult> searchVideos(String keyword) {
+        final long maxVideos = 25;
+        List<SearchResult> searchResultList = null;
+        try {
+            // Define the API request for retrieving search results.
+            YouTube.Search.List search = youtube.search().list("id,snippet");
+            search.setKey(apiKey);
+            search.setQ(keyword);
+
+            // Restrict the search results to only include videos. See:
+            // https://developers.google.com/youtube/v3/docs/search/list#type
+            search.setType("video");
+
+            // To increase efficiency, only retrieve the fields that the application uses.
+            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            search.setMaxResults(maxVideos);
+
+            // Call the API and print results
+            SearchListResponse searchResponse = search.execute();
+            searchResultList = searchResponse.getItems();
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+        } catch (IOException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        return searchResultList;
+    }
+
+    public List<String> listVideoTitles(Iterator<SearchResult> searchResult) {
+        List<String> titles = null;
+        if (searchResult != null) {
+            while (searchResult.hasNext()) {
+                SearchResult singleVideo = searchResult.next();
+                ResourceId resourceId = singleVideo.getId();
+                // Confirm that the result represents a video. Otherwise, the title will not be showed
+                if (resourceId.getKind().equals("youtube#video")) {
+                    titles.add(singleVideo.getSnippet().getTitle());
+                }
+            }
+        }
+        return titles;
+    }
+
+    public ResourceId getResourceId(SearchResult selectedResult) {
+        return selectedResult.getId();
+    }
+
+    public String getThumbnail(SearchResult selectedResult) {
+        Thumbnail thumbnail = selectedResult.getSnippet().getThumbnails().getDefault();
+        return thumbnail.getUrl();
+    }
+}
