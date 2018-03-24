b18a0bb3d0652619ad7019223cd14b9834491588
kammyWong <kammy609@msn.com>
2018-03-24 00:02:45 -0400

    #128 fix service problem

--- app/src/main/java/org/wikipedia/relatedvideos/YouTubeVideoService.java
+++ app/src/main/java/org/wikipedia/relatedvideos/YouTubeVideoService.java
@@ -1,4 +1,6 @@
-package org.wikipedia.youtubevideo;
+package org.wikipedia.relatedvideos;
+
+import android.util.Log;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpRequest;
@@ -26,25 +28,32 @@ public class YouTubeVideoService {
     private final String apiKey = "AIzaSyC97eTu0rdwGuoJg0hv1r8No_55iaaeBp4";
 
     public YouTubeVideoService() {
+        /*NetHttpTransport transport = new NetHttpTransport();
+        JacksonFactory jsonFactory = new JacksonFactory();
+        GoogleCredential credential = new GoogleCredential()
+                .setAccessToken(apiKey);
+        youtube = new YouTube.Builder(transport, jsonFactory,
+                credential).setApplicationName("Wikipedia-YouTube").build();*/
         youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
             @Override
             public void initialize(HttpRequest httpRequest) throws IOException {
             }
-        }).setYouTubeRequestInitializer(new YouTubeRequestInitializer(apiKey)).build();
+        }).setYouTubeRequestInitializer(new YouTubeRequestInitializer(apiKey)).setApplicationName("Wikipedia-YouTube").build();
     }
 
     public List<SearchResult> searchVideos(String keyword) {
+        Log.d("list", "keyword : " + keyword);
         final long maxVideos = 25;
         List<SearchResult> searchResultList = null;
         try {
             // Define the API request for retrieving search results.
             YouTube.Search.List search = youtube.search().list("id,snippet");
-            search.setKey(apiKey);
+            //search.setKey(apiKey);
             search.setQ(keyword);
 
             // Restrict the search results to only include videos. See:
             // https://developers.google.com/youtube/v3/docs/search/list#type
-            search.setType("video");
+            //search.setType("video");
 
             // To increase efficiency, only retrieve the fields that the application uses.
             search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
@@ -53,6 +62,8 @@ public class YouTubeVideoService {
             // Call the API and print results
             SearchListResponse searchResponse = search.execute();
             searchResultList = searchResponse.getItems();
+            Log.d("list", "Empty or not : " + search.isEmpty());
+            //Log.d("list", "searched result : " + searchResultList.get(0).getSnippet().getTitle());
         } catch (GoogleJsonResponseException e) {
             System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
                     + e.getDetails().getMessage());
@@ -73,6 +84,7 @@ public class YouTubeVideoService {
                 // Confirm that the result represents a video. Otherwise, the title will not be showed
                 if (resourceId.getKind().equals("youtube#video")) {
                     titles.add(singleVideo.getSnippet().getTitle());
+                    //titles.add(singleVideo.getSnippet().getDescription());
                 }
             }
         }
