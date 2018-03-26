6961f7db088048e2dd01a44df9b0c68124740c0a
Garrison Blair <garrisonblair96@gmail.com>
2018-03-25 20:28:43 -0400

    Issue #130: Added method to return the NotesEditFragment. Added code to display the body of the fragment. The NotesEditFragment was created to return the NotesEditFragment to the NotesFragment when the 'edit' button is pressed in order to launch this fragment.

--- app/src/main/java/org/wikipedia/notes/NotesEditFragment.java
+++ app/src/main/java/org/wikipedia/notes/NotesEditFragment.java
@@ -1,10 +1,14 @@
 package org.wikipedia.notes;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.wikipedia.R;
 import org.wikipedia.notebook.NoteReferenceService;
@@ -12,6 +16,7 @@ import org.wikipedia.notebook.NoteReferenceService;
 
 public class NotesEditFragment extends Fragment {
 
+    private SpannableString note;
     private String title;
     private int pageId;
     private NoteReferenceService noteReferenceService;
@@ -20,6 +25,14 @@ public class NotesEditFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i("DEBUG", "FRAGMENT CREATED");
+        if (getActivity().getIntent().getExtras() != null) {
+            Bundle bundleRead = getActivity().getIntent().getExtras();
+            title = bundleRead.getString("pageTitle");
+            pageId = bundleRead.getInt("pageId");
+            note = new SpannableString(bundleRead.getString("noteText"));
+        }
     }
 
     @Override
@@ -28,6 +41,20 @@ public class NotesEditFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_notes_edit, container, false);
 
+        TextView editTitle = view.findViewById(R.id.note_edit_title);
+        editTitle.setText(title);
+
+        TextView editBody = view.findViewById(R.id.note_edit_body);
+        editBody.setText(note);
+
+        Log.i("DEBUG", "VIEW CREATED");
+
         return view;
+    }
+
+    @NonNull
+    public static NotesEditFragment newInstance() {
+        NotesEditFragment fragment = new NotesEditFragment();
+        return fragment;
     }
 }
