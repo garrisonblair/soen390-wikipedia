e0d860ad237d966348821ef47eceab69d797cb24
Garrison Blair <garrisonblair96@gmail.com>
2018-03-27 16:18:50 -0400

    Issue #130: Changed the type of the text from SpannableString to SpannableStringBuilder. This is important for the future option of removing snippets of a note by the user. Added method that converts all the spans of a note into a string representation. This is done in order to save the span information in the database in order to persist the note markup.

--- app/src/main/java/org/wikipedia/notes/NotesEditFragment.java
+++ app/src/main/java/org/wikipedia/notes/NotesEditFragment.java
@@ -4,9 +4,11 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.CharacterStyle;
 import android.text.style.StyleSpan;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +22,7 @@ import org.wikipedia.notebook.NoteReferenceService;
 
 public class NotesEditFragment extends Fragment {
 
-    private SpannableString note;
+    private SpannableStringBuilder note;
     private String title;
     private int pageId;
     private NoteReferenceService noteReferenceService;
@@ -36,7 +38,7 @@ public class NotesEditFragment extends Fragment {
             pageId = bundleRead.getInt("pageId");
         }
         if (getArguments() != null) {
-            note = new SpannableString(getArguments().getString("note"));
+            note = new SpannableStringBuilder(getArguments().getString("note"));
         }
 
 //
@@ -62,7 +64,7 @@ public class NotesEditFragment extends Fragment {
                 if (editBody.getSelectionStart() != editBody.getSelectionEnd()) {
                     int start = editBody.getSelectionStart();
                     int end = editBody.getSelectionEnd();
-                    note.setSpan(new StyleSpan(Typeface.BOLD), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    note.setSpan(new StyleSpan(Typeface.BOLD), start, end, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
                     editBody.setText(note);
                 } else {
                     Toast.makeText(getContext(), "Select text to make bold first", Toast.LENGTH_SHORT).show();
@@ -78,7 +80,7 @@ public class NotesEditFragment extends Fragment {
                 if (editBody.getSelectionStart() != editBody.getSelectionEnd()) {
                     int start = editBody.getSelectionStart();
                     int end = editBody.getSelectionEnd();
-                    note.setSpan(new StyleSpan(Typeface.ITALIC), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    note.setSpan(new StyleSpan(Typeface.ITALIC), start, end, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
                     editBody.setText(note);
                 } else {
                     Toast.makeText(getContext(), "Select text to italicize first", Toast.LENGTH_SHORT).show();
@@ -94,7 +96,7 @@ public class NotesEditFragment extends Fragment {
                 if (editBody.getSelectionStart() != editBody.getSelectionEnd()) {
                     int start = editBody.getSelectionStart();
                     int end = editBody.getSelectionEnd();
-                    note.setSpan(new UnderlineSpan(), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    note.setSpan(new UnderlineSpan(), start, end, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
                     editBody.setText(note);
                 } else {
                     Toast.makeText(getContext(), "Select text to underline first", Toast.LENGTH_SHORT).show();
@@ -110,11 +112,55 @@ public class NotesEditFragment extends Fragment {
                 if (editBody.getSelectionStart() != editBody.getSelectionEnd()) {
                     int start = editBody.getSelectionStart();
                     int end = editBody.getSelectionEnd();
-                    note.setSpan(new StyleSpan(Typeface.NORMAL), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    note.setSpan(new StyleSpan(Typeface.NORMAL), start, end, SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
                     editBody.setText(note);
                 } else {
                     Toast.makeText(getContext(), "Select text to reset to normal first", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        // Re-setting selected text button
+        ImageButton done = view.findViewById(R.id.icon_done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String saved = new String();
+
+                int next;
+                for (int i = 0; i < note.length(); i = next) {
+
+                    int spanStart = i;
+                    // find the next span transition
+                    next = note.nextSpanTransition(i, note.length(), CharacterStyle.class);
+                    int spanEnd = next - 1;
+
+                    // get all spans in this range
+                    int numOfSpans = 0;
+                    String spanTypes = new String();
+                    CharacterStyle[] spans = note.getSpans(i, next, CharacterStyle.class);
+                    for (int j = 0; j < spans.length; j++) {
+                        if (spans[j] instanceof StyleSpan) {
+                            StyleSpan span = (StyleSpan) spans[j];
+                            if (span.getStyle() == 0) {
+                                spanTypes += ",n";
+                            }
+                            if (span.getStyle() == 1) {
+                                spanTypes += ",b";
+                            }
+                            if (span.getStyle() == 2) {
+                                spanTypes += ",i";
+                            }
+                        }
+                        if (spans[j] instanceof UnderlineSpan) {
+                            spanTypes += ",u";
+                        }
+                        numOfSpans++;
+                    }
+
+                    saved += "[" + spanStart + "," + spanEnd + "," + numOfSpans + spanTypes + "]";
+                }
+                Log.i("DEBUG SPANS", saved);
             }
         });
 
