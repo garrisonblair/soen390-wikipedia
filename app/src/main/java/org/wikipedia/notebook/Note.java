d881f8d19f3bcf68ea78da0838bc72876d7813eb
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-03-27 17:15:52 -0400

    #137 able the note class to handle the comments

--- app/src/main/java/org/wikipedia/notebook/Note.java
+++ app/src/main/java/org/wikipedia/notebook/Note.java
@@ -15,6 +15,7 @@ public class Note{
     private String articleTitle;
     private int articleid;
     private int id;
+    private String comment;
 
     public Note(int articleid, String articleTitle, String text) {
         this.originalText = text;
@@ -67,6 +68,18 @@ public class Note{
 
     public String getUpdatedText() {
         return this.updatedText;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void deleteComment() {
+        this.comment = null;
     }
 
     public void resetToOriginalText(){
