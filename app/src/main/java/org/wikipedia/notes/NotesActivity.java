cf5048b52f8ea08f473d9a890b44967505b67b48
Garrison Blair <garrisonblair96@gmail.com>
2018-03-27 22:29:51 -0400

    Issue #130: Added annotate method that builds a SpannableStringBuilder from a text string and a string with span information. This will be useful to load the saved notes with all the annotations.

--- app/src/main/java/org/wikipedia/notes/NotesActivity.java
+++ app/src/main/java/org/wikipedia/notes/NotesActivity.java
@@ -1,8 +1,11 @@
 package org.wikipedia.notes;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 
 import org.wikipedia.R;
 import org.wikipedia.WikipediaApp;
@@ -21,6 +24,63 @@ public class NotesActivity extends BaseActivity {
 
         setContentView(R.layout.activity_note);
         openNotesFragment();
+    }
+
+    public SpannableStringBuilder annotate(String note, String spans) {
+        SpannableStringBuilder annotatedNote = new SpannableStringBuilder(note);
+        int start;
+        String s;
+        int end;
+        String e;
+        int numSpans;
+        int i = 0;
+        while (i < spans.length()) {
+            i += 1; // Leave '['
+            s = "";
+            e = "";
+            while (spans.charAt(i) != '.') {
+                s += spans.charAt(i++);
+            }
+            i += 1; // Leave '.'
+            start = Integer.parseInt(s);
+            while (spans.charAt(i) != '.') {
+                e += spans.charAt(i++);
+            }
+            i += 1; // Leave '.'
+            end = Integer.parseInt(e);
+            numSpans = Integer.parseInt(String.valueOf(spans.charAt(i++)));
+            if (numSpans != 0) {
+                for (int j = 0; j < numSpans; j++) {
+                    switch (spans.charAt(i++)) {
+                        case 'b':
+                            annotatedNote.setSpan(
+                                    new StyleSpan(Typeface.BOLD),
+                                    start,
+                                    end,
+                                    SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            break;
+                        case 'i':
+                            annotatedNote.setSpan(
+                                    new StyleSpan(Typeface.ITALIC),
+                                    start,
+                                    end,
+                                    SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            break;
+                        case 'u':
+                            annotatedNote.setSpan(
+                                    new UnderlineSpan(),
+                                    start,
+                                    end,
+                                    SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            i += 1; // Leave ']'
+        }
+        return annotatedNote;
     }
 
     private void openNotesFragment() {
