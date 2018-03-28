dfa9da0cd334beaf4c711293c50f79ba1cff8d1e
Ken <kemoua@gmail.com>
2018-03-27 21:17:29 -0400

    #131 Created the table noteComments that will be used for the comments for a note

---
+++ app/src/main/java/org/wikipedia/notebook/database/NoteCommentsDao.java
@@ -0,0 +1,25 @@
+package org.wikipedia.notebook.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by Ken on 2018-03-27.
+ */
+@Dao
+public interface NoteCommentsDao {
+
+    @Query("SELECT * FROM `noteComments` WHERE noteId = :noteId ORDER BY ncid ASC")
+    List<NoteCommentsEntity> getAllNoteComments(int noteId);
+
+    @Insert
+    void addNoteComments(NoteCommentsEntity noteComment);
+
+    @Delete
+    void delete(NoteCommentsEntity noteComment);
+
+}
---
+++ app/src/main/java/org/wikipedia/notebook/database/NoteCommentsEntity.java
@@ -0,0 +1,44 @@
+package org.wikipedia.notebook.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+import static android.arch.persistence.room.ForeignKey.CASCADE;
+
+/**
+ * Created by Ken on 2018-03-27.
+ */
+
+@Entity(tableName = "noteComments",
+        foreignKeys = @ForeignKey(entity = NoteEntity.class,
+                                  parentColumns = "id",
+                                  childColumns = "noteId",
+                                  onDelete = CASCADE))
+public class NoteCommentsEntity {
+
+    @PrimaryKey (autoGenerate = true)
+    private int ncid;
+    private int noteId;
+    private String text;
+
+    public NoteCommentsEntity(int noteId, String text) {
+        this.noteId = noteId;
+        this.text = text;
+    }
+
+    public int getNcid() { return this.ncid; }
+
+    public int getNoteId() {
+        return this.noteId;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
