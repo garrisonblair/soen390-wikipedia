63bd033bf49196f7263acdfaa87021b6a40c5dc8
Garrison Blair <garrisonblair96@gmail.com>
2018-04-05 22:15:22 -0400

    Issue #142: Created endVisit method to calculate total time spent on article in visit and eventually save it to the database

--- app/src/main/java/org/wikipedia/userstatistics/StatReporter.java
+++ app/src/main/java/org/wikipedia/userstatistics/StatReporter.java
@@ -5,11 +5,13 @@ import java.util.Date;
 public class StatReporter {
 
     private int articleId;
-    private int timeSpent;
+    private long timeSpent;
     private Date start;
     private Date pause;
     private Date resume;
     private Date end;
+
+    private static final long DIV_BY_TO_GET_MINUTES = 60000;
 
     public void enterArticle(int articleId) {
         this.articleId = articleId;
@@ -22,6 +24,18 @@ public class StatReporter {
 
     public void resumeVisit() {
         resume = new Date();
+    }
+
+    public void endVisit() {
+        end = new Date();
+        if (pause != null && resume != null) {
+            long diff = pause.getTime() - start.getTime() + end.getTime() - resume.getTime();
+            timeSpent = diff / DIV_BY_TO_GET_MINUTES;
+        } else {
+            long diff = end.getTime() - start.getTime();
+            timeSpent = diff / DIV_BY_TO_GET_MINUTES;
+        }
+        // TODO: save timeSpent to db
     }
 
 }
