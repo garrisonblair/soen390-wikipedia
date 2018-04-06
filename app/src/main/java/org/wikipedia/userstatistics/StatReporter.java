3dabc8a7bf6b4c05af3b4928cc2871af70b633eb
Garrison Blair <garrisonblair96@gmail.com>
2018-04-05 22:00:31 -0400

    Issue #142 Created StatReporter class that will be used to collect information for statistics about article visits

---
+++ app/src/main/java/org/wikipedia/userstatistics/StatReporter.java
@@ -0,0 +1,19 @@
+package org.wikipedia.userstatistics;
+
+import java.util.Date;
+
+public class StatReporter {
+
+    private int articleId;
+    private int timeSpent;
+    private Date start;
+    private Date pause;
+    private Date resume;
+    private Date end;
+
+    public void enterArticle(int articleId) {
+        this.articleId = articleId;
+        start = new Date();
+    }
+
+}
