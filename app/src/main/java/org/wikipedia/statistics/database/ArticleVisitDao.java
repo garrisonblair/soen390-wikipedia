804218e638a8c32d00bbb690acacd39ab5ff4903
Steven Locke <tap3stry@gmail.com>
2018-04-07 14:28:30 -0400

    #151 Created empty entity and dao classes for ArticleVisit

---
+++ app/src/main/java/org/wikipedia/statistics/database/ArticleVisitDao.java
@@ -0,0 +1,8 @@
+package org.wikipedia.statistics.database;
+
+/**
+ * Created by steve on 07/04/18.
+ */
+
+public class ArticleVisitDao {
+}
---
+++ app/src/main/java/org/wikipedia/statistics/database/ArticleVisitEntity.java
@@ -0,0 +1,9 @@
+package org.wikipedia.statistics.database;
+
+/**
+ * Created by steve on 07/04/18.
+ */
+
+public class ArticleVisitEntity {
+
+}
