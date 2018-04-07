64becc0c81964c4380a978b145ec56bcda9c6836
Steven Locke <tap3stry@gmail.com>
2018-04-07 14:55:56 -0400

    #151 Created data attributes, constructor, and accessor/mutator methods for ArticleVisitEntity class

--- app/src/main/java/org/wikipedia/statistics/database/ArticleVisitEntity.java
+++ app/src/main/java/org/wikipedia/statistics/database/ArticleVisitEntity.java
@@ -1,9 +1,53 @@
 package org.wikipedia.statistics.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 /**
  * Created by steve on 07/04/18.
  */
 
+@Entity(tableName = "articleVisits")
 public class ArticleVisitEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int articleId;
+    private long timeSpentReading;
+
+    public ArticleVisitEntity(int id, int articleId, long timeSpentReading) {
+        this.id = id;
+        this.articleId = articleId;
+        this.timeSpentReading = timeSpentReading;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+
+    public int getArticleId() {
+        return this.articleId;
+    }
+
+    public void setTimeSpentReading(long timeSpentReading) {
+        this.timeSpentReading = timeSpentReading;
+    }
+
+    public long getTimeSpentReading() {
+        return this.timeSpentReading;
+    }
+
+
+
+
+
 
 }
