2a5cc49fa33f90e9cdce31e5dc306639345e2a3d
Garrison Blair <garrisonblair96@gmail.com>
2018-04-09 15:58:32 -0400

    Issue #142: Added attributes to StatCalculator class that will represent the different statistics.

--- app/src/main/java/org/wikipedia/userstatistics/StatCalculator.java
+++ app/src/main/java/org/wikipedia/userstatistics/StatCalculator.java
@@ -1,4 +1,24 @@
 package org.wikipedia.userstatistics;
 
+//import java.util.List;
+
 public class StatCalculator {
+
+//    private List<ArticleVisit> visitedArticles;
+
+    private long totalTimeSpentReading;
+    private long averageTimeSpentReading;
+    private long dailyTimeSpentReading;
+    private int longestReadArticleId;
+    private int totalArticlesRead;
+    private int uniqueArticlesRead;
+
+    private int totalNotes;
+    private int totalArticlesWithNotes;
+
+    public StatCalculator() {
+        // TODO: load article and note info
+    }
+
+    // TODO: calculate different statistics
 }
