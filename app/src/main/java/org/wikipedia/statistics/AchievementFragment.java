de7194ae6a916a5301c53ebf68634bd4b378e41a
xTEddie <edwardtranv1@gmail.com>
2018-04-09 21:11:12 -0400

    #152 Add basic achievement component setup

---
+++ app/src/main/java/org/wikipedia/statistics/AchievementFragment.java
@@ -0,0 +1,29 @@
+package org.wikipedia.statistics;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.wikipedia.R;
+
+public class AchievementFragment extends Fragment {
+
+    public static AchievementFragment newInstance() {
+        AchievementFragment fragment = new AchievementFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_achievement, container, false);
+        return view;
+    }
+
+}
--- app/src/main/java/org/wikipedia/statistics/StatisticFragment.java
+++ app/src/main/java/org/wikipedia/statistics/StatisticFragment.java
@@ -1,13 +1,15 @@
 package org.wikipedia.statistics;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import org.wikipedia.R;
 import org.wikipedia.views.SwipeableItemTouchHelperCallback;
@@ -15,9 +17,6 @@ import org.wikipedia.views.SwipeableItemTouchHelperCallback;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-/**
- * Created by KammyWong on 2018-04-05.
- */
 
 public class StatisticFragment extends Fragment {
 
@@ -36,6 +35,19 @@ public class StatisticFragment extends Fragment {
         ItemTouchHelper.Callback touchCallback = new SwipeableItemTouchHelperCallback(getContext());
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(touchCallback);
 
+        ImageButton achievementButton = view.findViewById(R.id.achievement_button);
+
+        achievementButton.setOnClickListener(v -> {
+            openAchievementPage();
+        });
+
         return view;
+
+    }
+
+    private void openAchievementPage() {
+        Log.d("DEV_DEBUG", "Should open achievement activity");
+        Intent intent = new Intent(getContext(), AchievementActivity.class);
+        startActivity(intent);
     }
 }
