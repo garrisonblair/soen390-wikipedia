662cb8bdeb0324aa1b7961f8c136e773011b6314
Ken <kemoua@gmail.com>
2018-04-10 17:56:31 -0400

    #153 Added the Achievement to the AppDatabase.

--- app/src/main/java/org/wikipedia/database/room/AppDatabase.java
+++ app/src/main/java/org/wikipedia/database/room/AppDatabase.java
@@ -9,16 +9,19 @@ import org.wikipedia.notebook.database.NoteDao;
 import org.wikipedia.notebook.database.NoteEntity;
 import org.wikipedia.notebook.database.ReferenceDao;
 import org.wikipedia.notebook.database.ReferenceEntity;
+import org.wikipedia.userstatistics.Database.AchievementDao;
+import org.wikipedia.userstatistics.Database.AchievementEntity;
 
 /**
  * Created by Andres on 2018-03-08.
  */
 
-@Database(entities = {NoteEntity.class, ReferenceEntity.class}, version = 1)
+@Database(entities = {NoteEntity.class, ReferenceEntity.class, AchievementEntity.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase{
     private static AppDatabase INSTANCE;
     public abstract NoteDao noteDao();
     public abstract ReferenceDao referenceDao();
+    public abstract AchievementDao achievementDao();
 
     public static AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
