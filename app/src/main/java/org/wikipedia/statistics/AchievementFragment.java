a4beed5b5a8cbd5d8cf126516ee87719c707ee31
xTEddie <edwardtranv1@gmail.com>
2018-04-09 22:35:26 -0400

    #152 Add basic listener to show sweetalert

--- app/src/main/java/org/wikipedia/statistics/AchievementFragment.java
+++ app/src/main/java/org/wikipedia/statistics/AchievementFragment.java
@@ -1,14 +1,22 @@
 package org.wikipedia.statistics;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.wikipedia.R;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+import static cn.pedant.SweetAlert.SweetAlertDialog.*;
+
 
 public class AchievementFragment extends Fragment {
+
+    private static final int SECRET_CLICK_LIMIT = 3;
+    private int mSecretClickCount;
 
     public static AchievementFragment newInstance() {
         AchievementFragment fragment = new AchievementFragment();
@@ -26,4 +34,20 @@ public class AchievementFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        getView().findViewById(R.id.fragment_achievement_title).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSecretClickCount++;
+
+                if(mSecretClickCount == SECRET_CLICK_LIMIT) {
+                    SweetAlertDialog dialog = new SweetAlertDialog(getContext(), SUCCESS_TYPE);
+                    dialog.setTitleText("Good job!");
+                    dialog.show();
+                    mSecretClickCount = 0;
+                }
+            }
+        });
+    }
 }
