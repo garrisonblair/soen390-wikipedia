d468740b37368be5ecf9c1632775e916ecb4438b
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-04-11 19:40:44 -0400

    #148 add numbers to the JourneyArticleHolder to visualize better the tree

--- app/src/main/java/org/wikipedia/journey/JourneyArticleHolder.java
+++ app/src/main/java/org/wikipedia/journey/JourneyArticleHolder.java
@@ -3,6 +3,7 @@ package org.wikipedia.journey;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -16,6 +17,7 @@ import org.wikipedia.views.ViewUtil;
  */
 
 public class JourneyArticleHolder extends TreeNode.BaseNodeViewHolder<Visit> {
+    private final int LEVEL_LEFT_MARGIN = 50;
 
     public JourneyArticleHolder(Context context) {
         super(context);
@@ -24,8 +26,14 @@ public class JourneyArticleHolder extends TreeNode.BaseNodeViewHolder<Visit> {
     @Override
     public View createNodeView(TreeNode node, Visit visit) {
         final LayoutInflater inflater = LayoutInflater.from(context);
-        final View view = inflater.inflate(R.layout.layout_profile_node, null, true);
+        final View view = inflater.inflate(R.layout.item_journey, null, true);
+        RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        node.setExpanded(true);
+        p.setMargins((node.getLevel() - 1) * LEVEL_LEFT_MARGIN, 0, 0, 0);
+        view.setLayoutParams(p);
+        TextView tvLevel = (TextView) view.findViewById(R.id.journey_level);
         TextView tvValue = (TextView) view.findViewById(R.id.node_value);
+        tvLevel.setText(node.getLevel() + ".");
         tvValue.setText(visit.getPageInfo().getDisplayTitle());
         SimpleDraweeView imageView = (SimpleDraweeView) view.findViewById(R.id.node_icon);
         ViewUtil.loadImageUrlInto(imageView, visit.getPageInfo().getLeadImageUrl());
