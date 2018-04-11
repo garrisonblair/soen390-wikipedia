fd753b078cdf03fd45f103efb65a3632a7798be4
Ken <kemoua@gmail.com>
2018-04-10 20:58:38 -0400

    #153 Add callback in AppDatabase to populate the achievements table on create.

--- app/src/main/java/org/wikipedia/database/room/AppDatabase.java
+++ app/src/main/java/org/wikipedia/database/room/AppDatabase.java
@@ -1,9 +1,11 @@
 package org.wikipedia.database.room;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.support.annotation.NonNull;
 
 import org.wikipedia.notebook.database.NoteDao;
 import org.wikipedia.notebook.database.NoteEntity;
@@ -11,6 +13,8 @@ import org.wikipedia.notebook.database.ReferenceDao;
 import org.wikipedia.notebook.database.ReferenceEntity;
 import org.wikipedia.userstatistics.Database.AchievementDao;
 import org.wikipedia.userstatistics.Database.AchievementEntity;
+
+import java.util.concurrent.Executors;
 
 /**
  * Created by Andres on 2018-03-08.
@@ -26,8 +30,16 @@ public abstract class AppDatabase extends RoomDatabase{
     public static AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    AppDatabase.class, "SOEN390-database").allowMainThreadQueries().build();
-            //TODO: Populate achievements table
+                    AppDatabase.class, "SOEN390-database")
+                    .addCallback(new Callback() {
+                                    @Override
+                                    public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                                        super.onCreate(db);
+                                        Executors.newSingleThreadScheduledExecutor().execute(() -> getInstance(context).achievementDao().addAllAchievements(AchievementEntity.populateData()));
+                                    }
+                                })
+                    .allowMainThreadQueries().build();
+
         }
         return INSTANCE;
     }
