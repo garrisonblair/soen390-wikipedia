a29505def596732a1c3aa5794a5677bb8418892c
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-04-11 11:56:46 -0400

    #148 make the JourneyArticleHolder able to display the article's main picture

--- app/src/main/java/org/wikipedia/journey/JourneyArticleHolder.java
+++ app/src/main/java/org/wikipedia/journey/JourneyArticleHolder.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.unnamed.b.atv.model.TreeNode;
 
 import org.wikipedia.R;
+import org.wikipedia.views.ViewUtil;
 
 /**
  * Created by Andres on 2018-04-10.
@@ -25,6 +27,8 @@ public class JourneyArticleHolder extends TreeNode.BaseNodeViewHolder<Visit> {
         final View view = inflater.inflate(R.layout.layout_profile_node, null, true);
         TextView tvValue = (TextView) view.findViewById(R.id.node_value);
         tvValue.setText(visit.getPageInfo().getDisplayTitle());
+        SimpleDraweeView imageView = (SimpleDraweeView) view.findViewById(R.id.node_icon);
+        ViewUtil.loadImageUrlInto(imageView, visit.getPageInfo().getLeadImageUrl());
 
         return view;
     }
