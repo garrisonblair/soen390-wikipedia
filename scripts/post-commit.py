9f809798bf96f7cc9cc3c77dfd448b0485fc5a75
Frederic Daigle <Freddydaigle@gmail.com>
2018-04-12 16:02:49 -0400

    #147 test

--- scripts/post-commit.py
+++ scripts/post-commit.py
@@ -5,4 +5,4 @@ with open('sonarqubeToken', 'r') as tokenFile:
     data=tokenFile.read().replace('\n', '')
 
 
-call([f"./gradlew sonarqube -Dsonar.host.url=http://localhost:9000   -Dsonar.login=$value"])
+call([f"./gradlew sonarqube -Dsonar.host.url=http://localhost:9000   -Dsonar.login={value}"])
