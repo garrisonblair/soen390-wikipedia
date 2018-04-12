26b4f02b0f2251fba9d96594db39404b6bdc3460
Frederic Daigle <Freddydaigle@gmail.com>
2018-04-12 16:33:16 -0400

    #147 added sonarQube parameter to indicate where Unit tests are located

--- scripts/post-commit.py
+++ scripts/post-commit.py
@@ -5,4 +5,4 @@ with open('scripts/sonarqubeToken', 'r') as tokenFile:
     data=tokenFile.read().replace('\n', '')
 
 
-subprocess.Popen(["./gradlew", "sonarqube", "-Dsonar.host.url=http://localhost:9000", "-Dsonar.login=" + data], close_fds=True)
+subprocess.Popen(["./gradlew", "sonarqube", "-Dsonar.host.url=http://localhost:9000", "-Dsonar.login=" + data, "-Dsonar.tests=app/src/test"], close_fds=True)
