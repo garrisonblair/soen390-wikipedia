cb758abfc4dffb3de00ef51efcd55ef6a0688fe1
Frederic Daigle <Freddydaigle@gmail.com>
2018-04-12 15:58:06 -0400

    #147 moving gradle call to python because it was not cross platform

---
+++ scripts/post-commit.py
@@ -0,0 +1,7 @@
+from subprocess import call
+
+data = ""
+with open('sonarqubeToken', 'r') as tokenFile:
+    data=tokenFile.read().replace('\n', '')
+
+call([f"./gradlew sonarqube -Dsonar.host.url=http://localhost:9000   -Dsonar.login=$value"])
