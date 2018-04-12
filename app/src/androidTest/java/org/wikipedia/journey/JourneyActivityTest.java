e3dc2e2d02ef549ae60a4e6835d7fe324813a165
ANDRES/Andres <a.nunez.zegarra@gmail.com>
2018-04-12 17:38:42 -0400

    #148 improve JourneyActivityTest, set delays for Espresso tests to run better on mobiles

--- app/src/androidTest/java/org/wikipedia/journey/JourneyActivityTest.java
+++ app/src/androidTest/java/org/wikipedia/journey/JourneyActivityTest.java
@@ -1,9 +1,11 @@
 package org.wikipedia.journey;
 
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.wikipedia.R;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -15,16 +17,17 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  * Created by Andres on 2018-04-12.
  */
 
+@RunWith(AndroidJUnit4.class)
 public class JourneyActivityTest {
 
     @Rule
-    public ActivityTestRule<JourneyActivity> activityRule = new ActivityTestRule(JourneyActivity.class);
+    public ActivityTestRule<JourneyActivity> journeyActivityRule = new ActivityTestRule(JourneyActivity.class);
 
     @Test
     public void emptyTreeTest() throws InterruptedException {
-        activityRule.getActivity();
-        Thread.sleep(2000);
+        journeyActivityRule.getActivity();
+        Thread.sleep(4000);
         onView(withId(R.id.nothing_to_display)).check(matches(isDisplayed()));
-        Thread.sleep(2000);
+        Thread.sleep(4000);
     }
 }
---
+++ app/src/androidTest/java/org/wikipedia/page/PageActivityTest.java
@@ -0,0 +1,8 @@
+package org.wikipedia.page;
+
+/**
+ * Created by Andres on 2018-04-12.
+ */
+
+public class PageActivityTest {
+}
