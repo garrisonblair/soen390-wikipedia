e9371743819d8cffc636e71c53d22fa48be9b5a8
xTEddie <edwardtranv1@gmail.com>
2018-04-12 15:39:13 -0400

    #152 Add custom adapter to show achievements

--- app/src/main/java/org/wikipedia/statistics/AchievementFragment.java
+++ app/src/main/java/org/wikipedia/statistics/AchievementFragment.java
@@ -3,6 +3,7 @@ package org.wikipedia.statistics;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -15,11 +16,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
 import org.wikipedia.R;
+import org.wikipedia.userstatistics.AchievementChecker;
 import org.wikipedia.userstatistics.AchievementService;
 import org.wikipedia.userstatistics.Database.AchievementEntity;
 
@@ -32,6 +36,50 @@ import static cn.pedant.SweetAlert.SweetAlertDialog.*;
 
 
 public class AchievementFragment extends Fragment {
+
+    private class CustomAdapter extends BaseAdapter {
+
+        private LayoutInflater inflater;
+        private ArrayList<AchievementEntity> objects;
+
+        private class ViewHolder {
+            TextView textViewTitle;
+            TextView textViewDescription;
+        }
+
+        public CustomAdapter(Context context, ArrayList<AchievementEntity> objects) {
+            inflater = LayoutInflater.from(context);
+            this.objects = objects;
+        }
+
+        public int getCount() {
+            return objects.size();
+        }
+
+        public AchievementEntity getItem(int position) {
+            return objects.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if(convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.achievement_row, null);
+                holder.textViewTitle = (TextView) convertView.findViewById(R.id.achievementRowTitle);
+                holder.textViewDescription = (TextView) convertView.findViewById(R.id.achievementRowDescription);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.textViewTitle.setText(objects.get(position).getName());
+            holder.textViewDescription.setText(objects.get(position).getDescription());
+            return convertView;
+        }
+    }
 
     private static final int SECRET_CLICK_LIMIT = 3;
     private int mSecretClickCount;
@@ -50,47 +98,20 @@ public class AchievementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_achievement, container, false);
 
-        ArrayList<String> achievements = new ArrayList();
-        ArrayList<String> unlockedAchievements = new ArrayList();
-        ArrayList<String> lockedAchievements = new ArrayList();
+        ArrayList<AchievementEntity> achievements = new ArrayList();
 
         AchievementService service = new AchievementService(getContext());
         service.getAllAchievements((AchievementService.GetAllAchievementsCallback) achievements1 -> {
-            for (AchievementEntity ach: achievements1) {
-                achievements.add(ach.getName());
-
-                if (ach.getChecked() == 1) {
-                    unlockedAchievements.add(ach.getName());
-                } else {
-                    lockedAchievements.add(ach.getName());
-                }
+            for (AchievementEntity achievementEntity: achievements1) {
+                achievements.add(achievementEntity);
             }
         });
 
         ListView achievementList = view.findViewById(R.id.achievement_list);
 
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.simple_row, achievements){
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent){
+        CustomAdapter customAdapter = new CustomAdapter(getContext(), achievements);
 
-                // Get the current item from ListView
-                View view = super.getView(position, convertView, parent);
-
-                if(position %2 == 1)
-                {
-                    TextView tv = (TextView) view;
-                    tv.setTextColor(Color.GREEN);
-//                    tv.setPadding(0, 0, 0, 0);
-                } else {
-                    TextView tv = (TextView) view;
-                    tv.setTextColor(Color.RED);
-//                    tv.setPadding(0, 0, 0, 0);
-                }
-                return view;
-            }
-        };
-
-        achievementList.setAdapter(arrayAdapter);
+        achievementList.setAdapter(customAdapter);
 
         return view;
     }
@@ -103,26 +124,28 @@ public class AchievementFragment extends Fragment {
                 mSecretClickCount++;
 
                 if(mSecretClickCount == SECRET_CLICK_LIMIT) {
-                    SweetAlertDialog dialog = new SweetAlertDialog(getContext(), SUCCESS_TYPE);
-                    dialog.setTitleText("Good job!");
-                    dialog.setConfirmText("See Achievements");
-                    dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                        @Override
-                        public void onClick(SweetAlertDialog sDialog) {
-                            sDialog.dismissWithAnimation();
+//                    SweetAlertDialog dialog = new SweetAlertDialog(getContext(), SUCCESS_TYPE);
+//                    dialog.setTitleText("Good job!");
+//                    dialog.setConfirmText("See Achievements");
+//                    dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+//                        @Override
+//                        public void onClick(SweetAlertDialog sDialog) {
+//                            sDialog.dismissWithAnimation();
+//
+//                            // Open achievements activity
+//                            Intent intent = new Intent(getContext(), AchievementActivity.class);
+//                            startActivity(intent);
+//                        }
+//                    });
+//                    dialog.setCancelButton("Close", new SweetAlertDialog.OnSweetClickListener() {
+//                        @Override
+//                        public void onClick(SweetAlertDialog sDialog) {
+//                            sDialog.dismissWithAnimation();
+//                        }
+//                    });
+//                    dialog.show();
+                    AchievementChecker checker = new AchievementChecker("Wikinerd", 1, getContext());
 
-                            // Open achievements activity
-                            Intent intent = new Intent(getContext(), AchievementActivity.class);
-                            startActivity(intent);
-                        }
-                    });
-                    dialog.setCancelButton("Close", new SweetAlertDialog.OnSweetClickListener() {
-                        @Override
-                        public void onClick(SweetAlertDialog sDialog) {
-                            sDialog.dismissWithAnimation();
-                        }
-                    });
-                    dialog.show();
                     mSecretClickCount = 0;
                 }
             }
