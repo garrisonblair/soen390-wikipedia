907654b6747a5648c999318e0d27a7494a4d24cd
Ken <kemoua@gmail.com>
2018-04-13 16:17:49 -0400

    #153 Added Unlock/Check achievements tests. Added test for the AssignmentChecker.

--- app/src/test/java/org/wikipedia/userstatistics/AchievementServiceTest.java
+++ app/src/test/java/org/wikipedia/userstatistics/AchievementServiceTest.java
@@ -14,11 +14,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.wikipedia.userstatistics.AchievementService.DeleteAchievementCallback;
 import static org.wikipedia.userstatistics.AchievementService.GetAllAchievementsCallback;
+import static org.wikipedia.userstatistics.AchievementService.UnlockAchievementCallback;
+import static org.wikipedia.userstatistics.AchievementService.DeleteAchievementCallback;
+import static org.wikipedia.userstatistics.AchievementService.CheckAchievementsCallback;
 
 /**
  * Created by Ken on 2018-04-12.
@@ -50,6 +57,24 @@ public class AchievementServiceTest {
     }
 
     @Test
+    public void unlockAchievementTest() {
+        UnlockAchievementCallback callback = mock(UnlockAchievementCallback.class);
+        Achievement achievementMock = mock(Achievement.class);
+
+        service.unlockAchievement(achievementMock, callback);
+        verify(achievementDaoMock).updateAchievement(any(AchievementEntity.class));
+    }
+
+    @Test
+    public void checkAchievementsTest() {
+        CheckAchievementsCallback callback = mock(CheckAchievementsCallback.class);
+        Achievement achievementMock = mock(Achievement.class);
+
+        service.checkAchievements(callback);
+        verify(achievementDaoMock).checkAchievements();
+    }
+
+    @Test
     public void getAllAchievementsTest() {
         GetAllAchievementsCallback callback = new GetAllAchievementsCallback() {
             @Override
@@ -67,5 +92,22 @@ public class AchievementServiceTest {
         service.getAllAchievements(callback);
         verify(achievementDaoMock).getAllAchievements();
         assertEquals(2, achievementDaoMock.getAllAchievements().size());
+    }
+
+    @Test
+    public void achievementCheckerTest() {
+        AchievementChecker checker = new AchievementChecker(contextMock);
+        AchievementChecker checkerSpy = spy(checker);
+
+        //unlocking is an update operation on the achievementDao, which is tested somewhere else.
+        doNothing().when(checkerSpy).unlockAchievement(anyString());
+
+        checkerSpy.check(AchievementsList.Note3, 100);
+        checkerSpy.check(AchievementsList.Note2, 2);
+        checkerSpy.check(AchievementsList.Note2, 6);
+
+        //unlockAchievement method should only be called once with the above parameters in the checkerSpy.check()
+        verify(checkerSpy, times(1)).unlockAchievement(AchievementsList.Note2.getName());
+
     }
 }
