b7e838fb503c6717c0116328af96efe19a1a11ab
xTEddie <edwardtranv1@gmail.com>
2018-04-12 22:47:26 -0400

    #154 Add logic to export journey

--- app/src/main/java/org/wikipedia/journey/JourneyActivity.java
+++ app/src/main/java/org/wikipedia/journey/JourneyActivity.java
@@ -2,6 +2,9 @@ package org.wikipedia.journey;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
@@ -9,6 +12,7 @@ import com.unnamed.b.atv.view.AndroidTreeView;
 import org.wikipedia.R;
 import org.wikipedia.activity.BaseActivity;
 import org.wikipedia.page.PageActivity;
+import org.wikipedia.util.ShareUtil;
 
 import java.util.List;
 
@@ -22,8 +26,27 @@ public class JourneyActivity extends BaseActivity {
         if (root.isLeaf()) {
             setContentView(R.layout.nothing_to_display_layout);
         } else {
+            setContentView(R.layout.activity_journey);
+
+            TextView tv = (TextView)findViewById(R.id.journey_title);
+            tv.setText("Journey");
+
+            LinearLayout linearLayout = findViewById(R.id.activity_journey);
+
+            // Add tree view as child
             AndroidTreeView treeView = new AndroidTreeView(this, root);
-            setContentView(treeView.getView());
+            linearLayout.addView(treeView.getView());
+
+            ImageButton shareJourneyButton = findViewById(R.id.journey_share_button);
+
+            // Add on click listener to share journey
+            shareJourneyButton.setOnClickListener(v -> {
+                JourneyRecorder journeyRecorder = JourneyRecorder.getInstance(this);
+                StringBuilder text = new StringBuilder();
+                String title = "Journey";
+                text.append(journeyRecorder.getJourneyString());
+                ShareUtil.shareText(this, title, text.toString());
+            });
         }
     }
 
